bebd00dbb23dadcccc0ecb246154935b542b6963
WarleyLeandro <warley.leandro95@gmail.com>
2023-08-20 22:55:22 -0300

    removed api log

--- src/services/app/getUsers.ts
+++ src/services/app/getUsers.ts
@@ -4,8 +4,6 @@ export async function getUsers() {
   try {
     const response = await api.get("/users?size=100");
 
-    console.log(response?.data);
-
     return response;
   } catch (error) {
     console.log("---> error: ");
